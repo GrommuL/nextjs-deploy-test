7a955dd203b8d446226a798fb4e504c2b35acf7e
GrommuL <miketsvg@gmail.com>
2024-08-12 17:58:01 +0500

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 const HomePage = () => {
   return (
     <div>
+      <div>ASDASDASDASDASDASDASD</div>
       <Image src='/wallpaper2.jpg' alt='asd' width={500} height={500} />
     </div>
   )
